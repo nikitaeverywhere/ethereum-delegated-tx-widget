f406122dad358b635c1195a4d61af43f70952e9c
Nikita Savchenko <zitros.lab@gmail.com>
2019-08-21 17:41:00 +0300

    Updates

---
+++ precache-manifest.654c7e31b8e00769b906f847f7f8a012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65d978fd0210601a16200e0f98bc664",
+    "url": "/ethereum-delegated-tx-widget/index.html"
+  },
+  {
+    "revision": "717b3e186081ad672c42",
+    "url": "/ethereum-delegated-tx-widget/static/css/main.0523f00a.chunk.css"
+  },
+  {
+    "revision": "1d63e17f28dc617fa57e",
+    "url": "/ethereum-delegated-tx-widget/static/js/2.4a2a81f1.chunk.js"
+  },
+  {
+    "revision": "717b3e186081ad672c42",
+    "url": "/ethereum-delegated-tx-widget/static/js/main.faed3ed1.chunk.js"
+  },
+  {
+    "revision": "4d0a2ae097843795d9fe",
+    "url": "/ethereum-delegated-tx-widget/static/js/runtime~main.f882906a.js"
+  }
+]);
