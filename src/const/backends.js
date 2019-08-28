df077d5fabbb24e3e83789de011d36aec7a095b1
Nikita Savchenko <zitros.lab@gmail.com>
2019-08-28 23:25:14 +0300

    Add mainnet back end URL

--- src/const/backends.js
+++ src/const/backends.js
@@ -1,5 +1,6 @@
 // The widget will try to get metadata from all back ends listed here.
 export const DELEGATED_TX_BACK_ENDS = [
   'http://localhost:8088',
+  'https://dapp.ml/delegated-tx-mainnet',
   'https://dapp.ml/delegated-tx'
 ];
