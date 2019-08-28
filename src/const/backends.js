33c143bc6f81b9003403ec96b2d9442a0670a145
Nikita Savchenko <zitros.lab@gmail.com>
2019-08-28 17:59:32 +0300

    Add public back end

--- src/const/backends.js
+++ src/const/backends.js
@@ -1,2 +1,5 @@
 // The widget will try to get metadata from all back ends listed here.
-export const DELEGATED_TX_BACK_ENDS = ['http://localhost:8088'];
+export const DELEGATED_TX_BACK_ENDS = [
+  'http://localhost:8088',
+  'https://dapp.ml/delegated-tx'
+];
