b5f40c2d1437340ded974c9c1d0db7f6e4458683
Nikita Savchenko <zitros.lab@gmail.com>
2019-09-16 18:17:10 +0300

    Add fallback back end URL

--- src/const/backends.js
+++ src/const/backends.js
@@ -1,6 +1,7 @@
 // The widget will try to get metadata from all back ends listed here.
 export const DEFAULT_DELEGATED_TX_BACK_ENDS = [
-  'http://localhost:8088',
+  'https://api-qa.dreamteam.gg/blockchain/delegated-tx',
+  'https://api.dreamteam.gg/blockchain/delegated-tx',
   'https://dapp.ml/delegated-tx-mainnet',
   'https://dapp.ml/delegated-tx'
 ];
