dff21c69b2f4c78a58d81ee0aa6835f1d5c78cba
Nikita Savchenko <zitros.lab@gmail.com>
2019-12-01 02:47:05 +0200

    Fix token icon because of the address is not normalized

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './TokenLogo.css';
+import { utils as ethersUtils } from 'ethers';
 
 export default function TokenLogo(props) {
   const tokenAddress =
     props.tokenAddress || '0x82f4ded9cec9b5750fbff5c2185aee35afc16587';
-  const assetUrl = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${tokenAddress}/logo.png`;
+  // ethersUtils.getAddress normalizes the address to a mixed case address
+  const assetUrl = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${ethersUtils.getAddress(tokenAddress)}/logo.png`;
   return <img className="token-logo-img" src={assetUrl} alt="" />;
 }
 
