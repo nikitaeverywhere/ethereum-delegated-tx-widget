e7b2f6c3f85c0fc9047bb0832231294972c688fb
Nikita Savchenko <zitros.lab@gmail.com>
2019-12-01 02:49:39 +0200

    Lint fix

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -7,7 +7,9 @@ export default function TokenLogo(props) {
   const tokenAddress =
     props.tokenAddress || '0x82f4ded9cec9b5750fbff5c2185aee35afc16587';
   // ethersUtils.getAddress normalizes the address to a mixed case address
-  const assetUrl = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${ethersUtils.getAddress(tokenAddress)}/logo.png`;
+  const assetUrl = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${ethersUtils.getAddress(
+    tokenAddress
+  )}/logo.png`;
   return <img className="token-logo-img" src={assetUrl} alt="" />;
 }
 
